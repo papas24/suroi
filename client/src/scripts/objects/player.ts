e0d15afa471f011a0b35db6d9caa1e4d720243f4
pap-24 <71824952+pap-24@users.noreply.github.com>
2024-05-05 13:38:00 +0300

    Final improvement for disguises code

--- client/src/scripts/objects/player.ts
+++ client/src/scripts/objects/player.ts
@@ -558,7 +558,7 @@ export class Player extends GameObject<ObjectCategory.Player> {
 
             // Check if it's a disguise. The frame will be set anyway.
             disguise.setVisible(skinDef.isDisguise);
-            disguise.setFrame(skinID);
+            disguise.setFrame(Loots.fromString<SkinDefinition>(skinID).obstacle);
 
             // In case it's a disguise, use a default skin so we have texture.
             body
