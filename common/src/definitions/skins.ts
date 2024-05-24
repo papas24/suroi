1d9195f4b1e56ff4b30da1a832e3476ae1b2d873
pap-24 <71824952+pap-24@users.noreply.github.com>
2024-05-24 20:48:19 +0300

    Removed useless stuff

--- common/src/definitions/skins.ts
+++ common/src/definitions/skins.ts
@@ -5,11 +5,14 @@ export interface SkinDefinition extends ItemDefinition {
     readonly hideFromLoadout: boolean
     readonly grassTint: boolean
     readonly hideEquipment: boolean
+
+    // -----------------------------------
     readonly isDisguise: boolean
-    readonly material: string
     readonly obstacle: string
     readonly obstacleSprite: string
     readonly explodes: boolean
+    // -----------------------------------
+
     readonly roleRequired?: string
 }
 
@@ -22,7 +25,6 @@ export const Skins = ObjectDefinitions.create<SkinDefinition>()(
             grassTint: false,
             hideEquipment: false,
             isDisguise: false,
-            material: undefined,
             obstacle: undefined,
             obstacleSprite: undefined,
             explodes: false
@@ -45,12 +47,11 @@ export const Skins = ObjectDefinitions.create<SkinDefinition>()(
         },
         disguise: {
             extends: "skin_factory",
-            applier: (obstacle: string, material: string, explodes = false) => ({
+            applier: (obstacle: string, explodes = false) => ({
                 explodes: explodes,
                 // In case of an obstacle with variations, replace the sliced part with nothing 
                 obstacle: ["rock", "oak_tree", "box"].includes(obstacle.replace(obstacle.slice(obstacle.length - 2), "")) ? obstacle.replace(obstacle.slice(obstacle.length - 2), "") : obstacle,
                 obstacleSprite: obstacle,
-                material: material,
                 isDisguise: true,
                 hideFromLoadout: true,
                 hideEquipment: true,
@@ -72,31 +73,31 @@ export const Skins = ObjectDefinitions.create<SkinDefinition>()(
         simple("with_role", ["composr"], ["Composr Swag"]),
 
         // Halloween Disguises
-        simple("disguise", ["regular_crate", "crate"], ["Guy in a Box"]),
-        simple("disguise", ["flint_crate", "crate"], ["Orange Boxman"]),
-        simple("disguise", ["aegis_crate", "crate"], ["Blue Boxman"]),
-        simple("disguise", ["grenade_crate", "crate"], ["Boombox"]),
-        simple("disguise", ["barrel", "metal", true], ["Fish in a Barrel"]),
-        simple("disguise", ["fridge", "appliance"], ["Indistructible"]),
-        simple("disguise", ["pine_tree", "tree"], ["The Lorax"]),
-        simple("disguise", ["rock_1", "stone"], ["Rock Solid"]),
-        simple("disguise", ["gold_rock", "stone"], ["Gold Solid"]),
-        simple("disguise", ["toilet", "porcelain"], ["Smelly"]),
-        simple("disguise", ["washing_machine", "appliance"], ["C-Cleaner"]),
-        simple("disguise", ["large_refinery_barrel", "metal", true], ["Large Suicide Bomber"]),
-        simple("disguise", ["stove", "metal", true], ["One With Cooking"]),
-        simple("disguise", ["dead_tree_big", "tree"], ["Spooky Barkskin"]),
-        simple("disguise", ["airdrop_crate", "crate"], ["Hot Drop"]),
-        simple("disguise", ["bush", "bush"], ["Bush Wookie"]),
-        simple("disguise", ["blueberry_bush", "bush"], ["Blueberry Bush Wookie"]),
-        simple("disguise", ["pumpkin", "pumpkin"], ["Pumpkin Head"]),
-        simple("disguise", ["tear_gas_crate", "crate", true], ["Tear Gas-r"]),
-        simple("disguise", ["super_barrel", "metal", true], ["Goldfish in a Barrel"]),
-        simple("disguise", ["table", "wood"], ["Simple Wood"]),
-        simple("disguise", ["rock_6", "stone"], ["Mossy"]),
-        simple("disguise", ["rock_7", "stone"], ["Cracked at Suroi"]),
-        simple("disguise", ["oak_tree_3", "tree"], ["Red Leaf"]),
-        simple("disguise", ["oak_tree_2", "tree"], ["Orange Leaf"]),
+        simple("disguise", ["regular_crate"], ["Guy in a Box"]),
+        simple("disguise", ["flint_crate"], ["Orange Boxman"]),
+        simple("disguise", ["aegis_crate"], ["Blue Boxman"]),
+        simple("disguise", ["grenade_crate"], ["Boombox"]),
+        simple("disguise", ["barrel", true], ["Fish in a Barrel"]),
+        simple("disguise", ["fridge"], ["Indistructible"]),
+        simple("disguise", ["pine_tree"], ["The Lorax"]),
+        simple("disguise", ["rock_1"], ["Rock Solid"]),
+        simple("disguise", ["gold_rock"], ["Gold Solid"]),
+        simple("disguise", ["toilet"], ["Smelly"]),
+        simple("disguise", ["washing_machine"], ["C-Cleaner"]),
+        simple("disguise", ["large_refinery_barrel", true], ["Large Suicide Bomber"]),
+        simple("disguise", ["stove", true], ["One With Cooking"]),
+        simple("disguise", ["dead_tree_big"], ["Spooky Barkskin"]),
+        simple("disguise", ["airdrop_crate"], ["Hot Drop"]),
+        simple("disguise", ["bush"], ["Bush Wookie"]),
+        simple("disguise", ["blueberry_bush"], ["Blueberry Bush Wookie"]),
+        simple("disguise", ["pumpkin"], ["Pumpkin Head"]),
+        simple("disguise", ["tear_gas_crate", true], ["Tear Gas-r"]),
+        simple("disguise", ["super_barrel", true], ["Goldfish in a Barrel"]),
+        simple("disguise", ["table"], ["Simple Wood"]),
+        simple("disguise", ["rock_6"], ["Mossy"]),
+        simple("disguise", ["rock_7"], ["Cracked at Suroi"]),
+        simple("disguise", ["oak_tree_3"], ["Red Leaf"]),
+        simple("disguise", ["oak_tree_2"], ["Orange Leaf"]),
        // simple("disguise", ["box_1", "cardboard"], ["Guy In Size 0"]),
        // simple("disguise", ["box_2", "cardboard"], ["Guy In Size 2"]),
        // simple("disguise", ["box_3", "cardboard"], ["Guy In Size 4"]),
